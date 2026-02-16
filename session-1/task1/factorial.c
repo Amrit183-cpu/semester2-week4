3c61ddbfc744da5d423f03ba0bca4a521f172d24
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-02-16 12:39:15 +0000

    week 4 s1

--- session-1/task1/factorial.c
+++ session-1/task1/factorial.c
@@ -6,7 +6,11 @@
  #include <stdio.h>
 
  int main( void ) {
-    int f[20];
+    int f[20]; /* this is the array*/
+    for ( int k=0; k<20; ++k) {
+      f[0] = 1;
+      f[k] = 
+    }
 
     /*
     Code to compute the factorial of each array index
--- session-1/task2/matrix_add.c
+++ session-1/task2/matrix_add.c
@@ -7,8 +7,31 @@
 
  int main( void ) {
     float a[4][4];
+    for (int k=0; k<4; ++k) {
+      for (int j=0; j<4; ++j){
+         a[k][j] = 1;
+         printf("%.2f",a[k][j]);
+      }
+      printf("\n ");
+    }
+
     float b[4][4];
+    for (int l=0; l<4; ++l) {
+      for (int m=0; m<4; ++m){
+         b[l][m] = 1;
+         printf("%.2f",b[l][m]);
+      }
+      printf("\n ");
+    }
     float c[4][4];
+
+    for (int x=0; x<4; ++x){
+      for (int y=0; y<4; ++y) {
+         c[x][y] = ("%.2f ",a[x][y] + b[x][y]);
+         printf("%.2f",c[x][y]);
+      }
+      printf("\n");
+    }
 
     /*
     Intialise the matrix a and b entries to 1. 

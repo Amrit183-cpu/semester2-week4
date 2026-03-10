9c987aabcbf8c7e6e5ff86be1aa44607bf172d98
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-03-10 11:39:59 +0000

    week 2.4 task1 fin

--- session-1/task1/dot_product.c
+++ session-1/task1/dot_product.c
@@ -10,10 +10,11 @@
     float b[]={ 2.0,3.0,4.0,5.0,6.0 };
     float d;
 
-    /*
-    Code to compute the dot product
-    Store your answer in d
-    print your final answer
-    */
+   size_t a_length = sizeof(a) / sizeof(a[0]);
+    for (int i=0; i<a_length; ++i){
+      d = d + (a[i] * b[i]);
+    }
+    printf("%f", d);
+    
     return 0;
  }
--- session-1/task1/vector_add.c
+++ session-1/task1/vector_add.c
@@ -10,10 +10,14 @@
     float b[]={ 2.0,3.0,4.0,5.0,6.0 };
     float c[5];
 
-    /*
-    Code to compute the sum
-    Store your answer in array c
-    Print your final answer
-    */
+    size_t a_length = sizeof(a) / sizeof(a[0]);
+    size_t b_length = sizeof(b) / sizeof(b[0]);
+
+    for (int i=0; i<a_length; ++i){
+      int result = a[i] + b[i];
+      c[i] = result;
+      printf("%f \n", c[i]);
+    }
+
     return 0;
  }

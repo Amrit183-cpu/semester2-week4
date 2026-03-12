13a243ea8f9bbdd406e5d741bfde40b96a623158
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-03-12 20:19:34 +0000

    2.4 worksheet

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -2,18 +2,6 @@
 #include <stdio.h>
 
 int main( void ) {
-
-    /*
-    // define suitable data
-    int a, b;
-
-    // use scanf to read from the terminal
-    int count = scanf("%d + %d =", &a, &b);
-
-    // print the output from scanf and the data values
-    printf("%d + %d = %d \n", a, b, (a+b));
-    */
-
     // define suitable data
     char nums[30]; //array size 30
     int num1;
--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,18 +1,45 @@
 
 /*
-Name:
-Student ID:
+Name: Amritpal Singh
+Student ID: 201965262
 */
 
 #include <stdio.h>
 #include <string.h>
 
 int main( int argc, char **argv ) {
-
+    
     // define appropriate data to hold your answer
+    char buffer[100]; //array storing 100 chars
+    int count = 0; //acts as SP in array
+
+    //check if amount of arguments is <=10
+    if (argc<12){
+        //check all args are <=4 chars
+        for (int k=1; k<argc; k++){ //start from 1 due to name of program
+            if (strlen(argv[k]) >4){ //if length of arg >4
+                return 0; //end program
+            }
+            else {
+                if (k<argc-1){ //only do for not last args
+                    for (int i=0; i<strlen(argv[k]); i++){ //loop over each char in this element
+                        buffer[count] = argv[k][i]; //append buffer array
+                        count = count + 1; //increment counter
+                    }
+                    buffer[count] = '-'; //only add after every arg
+                    count = count + 1; //increment counter
+                } else { //for last argument don't add '-'
+                    for (int x=0; x<strlen(argv[k]); x++){ //loop over each char in this element
+                        buffer[count] = argv[k][x]; //append buffer array
+                        count = count + 1; //increment counter
+                    }
+                }
+            }
+        }
+    }
 
     // process the command-line data using appropriate string functions
-
+    buffer[count] = '\0'; //end the string 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;

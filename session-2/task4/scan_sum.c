5e415c7ccb65c01ad3c2c7b8f8b7f83b9f4eedf5
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-03-12 12:33:09 +0000

    week 2.4 task 4

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -3,10 +3,30 @@
 
 int main( void ) {
 
+    /*
     // define suitable data
+    int a, b;
 
     // use scanf to read from the terminal
-    // print the output from scanf and the data values 
+    int count = scanf("%d + %d =", &a, &b);
+
+    // print the output from scanf and the data values
+    printf("%d + %d = %d \n", a, b, (a+b));
+    */
+
+    // define suitable data
+    char nums[30]; //array size 30
+    int num1;
+    int num2;
+
+    //read userinput
+    fgets(nums, sizeof(nums), stdin);
+
+    // use sscanf to read from the terminal
+    sscanf(nums, "%d + %d =", &num1, &num2);
+
+    // print the output from scanf and the data values
+    printf("%d + %d = %d \n", num1, num2, (num1+num2));
 
     return 0;
 }
